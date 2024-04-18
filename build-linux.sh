0338ac7e37b606caf703c5a842df7300d916c436
Christopher Whitley <aristurtledev@gmail.com>
2024-04-17 20:02:15 -0400

    build dependencies

--- build-linux.sh
+++ build-linux.sh
@@ -1,74 +1,78 @@
 #!/bin/bash
 
 ROOT_DIR="$(pwd)"
-SHARED_CONFIG="--enable-static \
-               --disable-shared \
-               --disable-encoders \
-               --enable-encoder=pcm_u8 \
-               --enable-encoder=pcm_f32le \
-               --enable-encoder=pcm_s16le \
-               --enable-encoder=adpcm_ms \
-               --enable-encoder=wmav2 \
-               --enable-encoder=adpcm_ima_wav \
-               --enable-encoder=aac \
-               --enable-libvorbis \
-               --enable-libmp3lame \
-               --enable-encoder=mp4 \
-               --disable-decoders \
-               --enable-decoder=pcm_u8 \
-               --enable-decoder=pcm_f32le \
-               --enable-decoder=pcm_s16le \
-               --enable-decoder=adpcm_ms \
-               --enable-decoder=wmav2 \
-               --enable-decoder=adpcm_ima_wav \
-               --enable-decoder=aac \
-               --enable-decoder=libvorbis \
-               --enable-decoder=libmp3lame \
-               --enable-decoder=mp4 \
-               --disable-muxers \
-               --enable-muxer=wav \
-               --enable-muxer=asf \
-               --enable-muxer=ipod \
-               --enable-muxer=ogg \
-               --enable-muxer=mp3"
+DEPENDENCIES_DIR="$ROOT_DIR/dependencies"
+ARTIFACTS_DIR="$ROOT_DIR/artifacts-linux"
+FFMPEG_DIR="$ROOT_DIR/ffmpeg"
 
-# Install general dependencies
+# Install general build tools
 sudo apt-get update
 sudo apt-get install -y build-essential pkg-config yasm git
 
-# Install library dependencies
-sudo apt-get install -y libx264-dev libx265-dev libvorbis-dev libmp3lame-dev lame
+# Create directories
+mkdir -p "$DEPENDENCIES_DIR"
+mkdir -p "$ARTIFACTS_DIR"
+
+# Build and install libvorbis
+cd "$DEPENDENCIES_DIR"
+git clone https://gitlab.xiph.org/xiph/vorbis.git libvorbis
+cd libvorbis
+./autogen.sh
+./configure --prefix="$ARTIFACTS_DIR" --enable-static --disable-shared
+make
+make install
+
+# Build and install libmp3lame
+cd "$DEPENDENCIES_DIR"
+wget https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz
+tar -xf lame-3.100.tar.gz
+cd lame-3.100
+./configure --prefix="$ARTIFACTS_DIR" --enable-static --disable-shared
+make
+make install
 
 # Clone the ffmpeg repository
+cd "$ROOT_DIR"
 git clone --depth 1 --branch n7.0 https://github.com/ffmpeg/ffmpeg.git
 
-cd ffmpeg
-./configure $SHARED_CONFIG
-make -j$(nproc)
+# Compile FFmpeg
+cd "$FFMPEG_DIR"
+./configure --prefix="$ARTIFACTS_DIR" --enable-static \
+            --disable-shared \
+            --disable-encoders \
+            --enable-encoder=pcm_u8 \
+            --enable-encoder=pcm_f32le \
+            --enable-encoder=pcm_s16le \
+            --enable-encoder=adpcm_ms \
+            --enable-encoder=wmav2 \
+            --enable-encoder=adpcm_ima_wav \
+            --enable-encoder=aac \
+            --enable-libvorbis \
+            --enable-libmp3lame \
+            --enable-encoder=mp4 \
+            --disable-decoders \
+            --enable-decoder=pcm_u8 \
+            --enable-decoder=pcm_f32le \
+            --enable-decoder=pcm_s16le \
+            --enable-decoder=adpcm_ms \
+            --enable-decoder=wmav2 \
+            --enable-decoder=adpcm_ima_wav \
+            --enable-decoder=aac \
+            --enable-decoder=libvorbis \
+            --enable-decoder=libmp3lame \
+            --enable-decoder=mp4 \
+            --disable-muxers \
+            --enable-muxer=wav \
+            --enable-muxer=asf \
+            --enable-muxer=ipod \
+            --enable-muxer=ogg \
+            --enable-muxer=mp3
+make
+make install
 
-cd "$ROOT_DIR"
-mkdir artifacts-linux
-cp ./ffmpeg/ffmpeg ./artifacts-linux
+echo "FFmpeg compiled and installed with static dependencies."
 
-# Move to artifacts-linux directory
-cd ./artifacts-linux
-
-# Execute ldd on the ffmpeg binary and extract the library dependencies
-ldd_output=$(ldd ffmpeg)
-
-# Loop through each line of ldd output
-while IFS= read -r line; do
-    # Extract the library path from each line
-    lib_path=$(echo "$line" | awk '{print $(NF-1)}')
-
-    # Check if the library path is within /lib, /lib64, or /usr/lib
-    if [[ $lib_path =~ ^(/lib|/lib64|/usr/lib) ]]; then
-        # Extract the library name
-        lib_name=$(echo "$line" | awk '{print $1}')
-
-        # Copy the library to the artifacts-linux directory
-        cp --parents "$lib_name" .
-    fi
-done <<< "$ldd_output"
-
-echo "ffmpeg binary dependencies copied to artifacts-linux directory."
+# Print ldd output for FFmpeg binary
+cd "$ARTIFACTS_DIR"
+echo "ldd ffmpeg result:"
+ldd ffmpeg
